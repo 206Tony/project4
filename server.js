7e77dca98fa55b2089ae8079104b582f4c75b9bb
Anthony Zardis <anthonyzardis@gmail.com>
2019-09-15 17:29:51 -0700

    heroku db

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(helmet());
-app.use(express.static(__dirname + '/client/build'));  // from drinks
-app.use(methodOverride('_method'));  //from drinks
+app.use(express.static(__dirname + '/client/build')); 
+app.use(methodOverride('_method'));
 
 const loginLimiter = new RateLimit({
   windowMs: 5*60*1000,
@@ -28,7 +28,7 @@ const signupLimiter = new RateLimit({
   message: "Maximum accounts created. Please try again later."
 })
 
-mongoose.connect('mongodb://localhost/lego', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.once('open', () => {
   console.log(`Connected to Mongo on ${db.host}:${db.port}`);
@@ -37,14 +37,14 @@ db.on('error', (err) => {
   console.log(`Database error:\n${err}`);
 });
 
-// app.use('/auth/login', loginLimiter);
-// app.use('/auth/signup', signupLimiter);
+app.use('/auth/login', loginLimiter);
+app.use('/auth/signup', signupLimiter);
 
 app.use('/auth', require('./routes/auth'));
 app.use('/api', expressJWT({secret: process.env.JWT_SECRET}),  require('./routes/api')); 
 
 app.get('*', function(req, res) {
-	res.sendFile(__dirname + '/client/build/index.html');  // from drinks
+	res.sendFile(__dirname + '/client/build/index.html');
 });
 
 app.listen(process.env.PORT, () => {
